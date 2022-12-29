554fa2e90d7c9536203b6bb4759009557cb18ada
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2022-12-29 08:42:51 -0500

    dev: icon updates

---
+++ src/lib/components/atoms/IconPause.svelte
@@ -0,0 +1,17 @@
+<!--
+@component
+pause icon
+
+-->
+
+<script lang='ts'>
+
+  // props
+  //export let colo = false
+
+</script>
+
+<template lang='pug'>
+svg(height="14" width="14" viewBox="0 0 14 14" style="stroke:currentColor; stroke-width:1; width:100%; height:auto;")
+	path(d="M3.5 0 L3.5 14 M10.5 0 L10.5 14")
+</template>
---
+++ src/lib/components/atoms/IconPlay.svelte
@@ -0,0 +1,11 @@
+
+<!--
+@component
+play icon
+* stroke = currentColor
+-->
+
+<template lang='pug'>
+svg(height="14" width="14" viewBox="0 0 14 14" style="fill:currentColor; stroke-width:0; width:100%; height:auto;")
+  polygon(points="0,0 12,7 0,14" style="fill:currentColor;")
+</template>
